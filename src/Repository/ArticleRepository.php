6e8447520d12243eb5bd22e1db607d81c26e1b3e
Petervrancken <peter.vrancken88@gmail.com>
2021-03-31 14:00:59 +0200

    doctrine: Reusing Query Logic & Param Converters

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -4,6 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Article;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -24,12 +25,23 @@ class ArticleRepository extends ServiceEntityRepository
     //  */
     public function findAllPublishedOrderedByNewest()
     {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.publishedAt IS NOT NULL')
+        return $this->addIsPublishedQueryBuilder()
             ->orderBy('a.publishedAt', 'DESC')
             ->getQuery()
             ->getResult()
         ;
+    }
+
+    private function addIsPublishedQueryBuilder(QueryBuilder $qb = null)
+    {
+        return $this->getOrCreateQueryBuilder($qb)
+                    ->andWhere('a.publishedAt IS NOT NULL');
+    }
+
+    private function getOrCreateQueryBuilder( QueryBuilder $qb = null)
+    {
+        if ($qb == null) {$qb = $this->createQueryBuilder('a');}
+        return $qb;
     }
 
     /*
