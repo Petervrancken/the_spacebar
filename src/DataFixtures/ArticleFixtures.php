52c6f53db3bcd56b485e313d9a0c877cf4c2140f
Petervrancken <peter.vrancken88@gmail.com>
2021-03-31 19:16:17 +0200

    doctrine: Using Faker

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -7,25 +7,43 @@ use Doctrine\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixtures
 {
+    private static $articleTitles = [
+        'Why Asteroids Taste Like Bacon',
+        'Life on Planet Mercury: Tan, Relaxing and Fabulous',
+        'Light Speed Travel: Fountain of Youth or Fallacy',
+    ];
+
+    private static $articleImages = [
+        'asteroid.jpeg',
+        'mercury.jpeg',
+        'lightspeed.png',
+    ];
+
+    private static $articleAuthors = [
+        'Mike Ferengi',
+        'Amy Oort',
+    ];
+
+
     public function loadData(ObjectManager $manager)
     {
         $this->createMany(Article::class, 10, function( Article $a, $count ){
 
-            $a->setTitle("Why asteroids taste like bacon" . $count);
-            $a->setSlug("why-asteroids-taste-like-bacon" . $count );
+            $a->setTitle($this->faker->randomElement(self::$articleTitles) );
+            $a->setSlug($this->faker->slug );
             $a->setContent(<<<EOF
     Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow, lorem proident …
     Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck fugiat.
 EOF
             );
 
-            if ( rand(1,10) > 2 ){
-                $a->setPublishedAt( new \DateTime( sprintf('-%d days', rand(1,100))));
+            if ( $this->faker->boolean(70)){
+                $a->setPublishedAt( $this->faker->dateTimeBetween('-100 days', '-1 day'));
             }
 
-            $a->setAuthor("Mike Ferengi")
-                ->setHeartCount( rand(5,100))
-                ->setImageFilename('asteroid.jpeg');
+            $a->setAuthor($this->faker->randomElement(self::$articleAuthors))
+                ->setHeartCount( $this->faker->numberBetween(5, 100))
+                ->setImageFilename($this->faker->randomElement(self::$articleImages));
         });
 
         $manager->flush();
--- src/DataFixtures/BaseFixtures.php
+++ src/DataFixtures/BaseFixtures.php
@@ -6,23 +6,32 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Generator;
 
 abstract class BaseFixtures extends Fixture
 {
     /** @var ObjectManager */
     private $manager;
 
+    /**
+     * @var Generator
+     */
+    protected $faker;
+
     abstract protected function loadData(ObjectManager $manager);
 
     public function load(ObjectManager $manager)
     {
         $this->manager = $manager;
+        $this->faker = Factory::create();
         $this->loadData($manager);
     }
 
     protected function createMany(string $className, int $count, callable $factory)
     {
-        for ($i = 0; $i < $count; $i++) {
+        for ($i = 0; $i < $count; $i++)
+        {
             $entity = new $className;
             $factory($entity, $i);
 
@@ -32,4 +41,5 @@ abstract class BaseFixtures extends Fixture
         }
     }
 }
+
 
